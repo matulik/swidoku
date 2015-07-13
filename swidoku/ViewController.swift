6bcf25b619f48229cfa09012632792fb4163747f
matulik <matulik>
2015-07-13 13:54:16 +0200

    Model/View Set/Get method added

--- swidoku/ViewController.swift
+++ swidoku/ViewController.swift
@@ -9,21 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var sq00View: UIView!
-    @IBOutlet var sq01View: UIView!
-    @IBOutlet var sq02View: UIView!
-    @IBOutlet var sq10View: UIView!
-    @IBOutlet var sq11View: UIView!
-    @IBOutlet var sq12Vuew: UIView!
-    @IBOutlet var sq20View: UIView!
-    @IBOutlet var sq21View: UIView!
-    @IBOutlet var sq22View: UIView!
-
+    @IBOutlet var contentView: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,8 +23,62 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    //Method to get XY value from view
     func getXYViewValue(x: Int, y: Int) -> Int {
-        var label00 : UILabel = self.sq00View.viewWithTag(0)
+        // Index for 3x3
+        var sindex : Int = -1
+        // Index for 9x9
+        var bindex : Int = -1
+        if (x == 0 && y == 0) {
+            sindex = 99
+            bindex = 100
+        }
+        else {
+            bindex = (x*10)+y
+            sindex = (x+1/3)*10+(y+1/3)
+        }
+        // Getting value
+        println("get: x=\(sindex), y=\(bindex)")
+        var label : UILabel? = self.contentView.viewWithTag(sindex)?.viewWithTag(bindex) as? UILabel
+        if let l = label {
+            return l.text!.toInt()!
+        }
+        // Bad x or y = -1
+        else {
+            return -1
+        }
+    }
+    
+    // Method to setting value in view
+    func setXYViewValue(x: Int, y: Int, v: Int) -> Int {
+        // Bad value = -2
+        if (v < 0 || v > 9) {
+            return -2
+        }
+        // Index for 3x3
+        var sindex : Int = -1
+        // Index for 9x9
+        var bindex : Int = -1
+        if (x == 0 && y == 0) {
+            sindex = 99
+            bindex = 100
+        }
+        else {
+            bindex = (x*10)+y
+            sindex = (x+1/3)*10+(y+1/3)
+        }
+        // Setting value
+        println("set: x=\(sindex), y=\(bindex)")
+        var label : UILabel? = self.contentView.viewWithTag(sindex)?.viewWithTag(bindex) as? UILabel
+        if let l = label {
+            l.text = String(format: "%d", v)
+            // Return 0 if success
+            return 0
+        }
+            // Bad x or y = -1
+        else {
+            return -1
+        }
     }
 }
-
